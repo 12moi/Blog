aae474b1ebeceda54d56c306c2c0298786594d0e
12moi <12moi>
2022-03-16 22:35:45 +0300

    add update

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 
 
 export SECRET_KEY='my1212mk'
-export SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:12moi12@localhost/blog'
+export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:12moi12@localhost/blog'
 export MAIL_USERNAME="moi.shadrack@student.moringaschool.com"
 export MAIL_PASSWORD='MkelorD12'
 
